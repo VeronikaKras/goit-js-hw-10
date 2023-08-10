bbc78cae62fcfd5ea4dd3f52ade081b96387d9f0
VeronikaKras <127940482+VeronikaKras@users.noreply.github.com>
2023-08-10 17:48:40 +0000

    Deploying to gh-pages from @ VeronikaKras/goit-js-hw-10@9c1cfe5da1b5ed6b18290c386aa359e76a651c69 🚀

--- index.1a87f172.js
+++
@@ -1,2 +0,0 @@
-!function(){axios.defaults.headers.common["x-api-key"]="live_wMLksS0GuaQqPVH3u1VLBvmUZAbMJ8e3AegBJO0UW7Vryje54J8fOwWNJInxbJPV";console.log(12123)}();
-//# sourceMappingURL=index.1a87f172.js.map
