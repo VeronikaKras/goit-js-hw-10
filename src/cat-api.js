023af3cf68536667477c373cce61bacbdace459f
Veronika <nikakras.dance@gmail.com>
2023-08-10 13:51:17 +0300

    start

---
+++ src/cat-api.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.headers.common["x-api-key"] = "live_wMLksS0GuaQqPVH3u1VLBvmUZAbMJ8e3AegBJO0UW7Vryje54J8fOwWNJInxbJPV";
+
+fetchBreeds("https://api.thecatapi.com/v1/breeds");
