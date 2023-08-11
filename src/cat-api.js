4cec6dc7352c96027d2ab94062c4f2cfdd4c2a9e
Veronika <nikakras.dance@gmail.com>
2023-08-11 13:40:19 +0300

    final

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,9 +1,16 @@
 import axios from 'axios';
 
-axios.defaults.headers.common['x-api-key'] =
-  'live_wMLksS0GuaQqPVH3u1VLBvmUZAbMJ8e3AegBJO0UW7Vryje54J8fOwWNJInxbJPV';
 
-export const getBreeds = qwe => {
-  return qwe;
-};
-// fetchBreeds("https://api.thecatapi.com/v1/breeds");
+export function fetchBreeds() {
+    axios.defaults.headers.common["x-api-key"] = "live_wMLksS0GuaQqPVH3u1VLBvmUZAbMJ8e3AegBJO0UW7Vryje54J8fOwWNJInxbJPV";
+    return axios.get('https://api.thecatapi.com/v1/breeds').then(responce => {
+        return responce;
+    });
+}
+
+export function fetchCatByBreed(breedId){
+    axios.defaults.headers.common["x-api-key"] = "live_wMLksS0GuaQqPVH3u1VLBvmUZAbMJ8e3AegBJO0UW7Vryje54J8fOwWNJInxbJPV";
+    return axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`).then(responce => {
+        return responce;
+    });
+}
